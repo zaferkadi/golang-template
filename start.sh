9daa293a18a18d7784511b9458c46a2b4f01551d
zafer <zafer.qadi@gmail.com>
2021-11-16 00:01:02 +0100

    Add docker-compose

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e # Exit immediately if a command exits with a non-zero status.
+
+echo "run db migration ..."
+/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+
+echo "start the app"
+exec "$@" # Execute the command that was passed to docker run.
+
+
